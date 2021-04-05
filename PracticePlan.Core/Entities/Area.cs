4476edf659b86d152c381b3ed30127ce4db9babf
olo42 <olo@apck.de>
2021-04-05 20:20:23 +0200

    Add Area entity

---
+++ PracticePlan.Core/Entities/Area.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace PracticePlan.Core.Entities
+{
+  public class Area
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<Contact> Contacts { get; set; }
+    public Address Address { get; set; }
+  }
+}
