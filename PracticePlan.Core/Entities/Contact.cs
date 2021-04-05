74ee25a4257eb559c69377a86ca015134fe755ae
olo42 <olo@apck.de>
2021-04-05 20:17:27 +0200

    Add Address and Contact entities

---
+++ PracticePlan.Core/Entities/Contact.cs
@@ -0,0 +1,13 @@
+﻿namespace PracticePlan.Core.Entities
+{
+  public class Contact
+  {
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Mobile { get; set; }
+    public Address Address { get; set; }
+  }
+}
