71b2628745c0e809d10fbc4f77b36fa0c3049492
olo42 <olo@apck.de>
2021-04-05 20:24:36 +0200

    Add Schedule and Appointment entity

---
+++ PracticePlan.Core/Entities/Appointment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticePlan.Core.Entities
+{
+  public class Appointment
+  {
+    public int Id { get; set; }
+    public DateTime StartDateTime { get; set; }
+    public DateTime EndDateTime { get; set; }
+    public Area Area { get; set; }
+  }
+}
---
+++ PracticePlan.Core/Entities/Schedule.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace PracticePlan.Core.Entities
+{
+  public class Schedule
+  {
+    public int Id { get; set; }
+    public string Description { get; set; }
+    public List<Appointment> Appointments { get; set; }
+  }
+}
